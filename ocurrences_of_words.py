568c4bcba8f825788cd19ff6233fca2c583f99ba
CARLOS-PC <carlos.salgar1@my.jcu.edu.au>
2016-09-04 19:48:35 +1000

    finished

--- ocurrences_of_words.py
+++ ocurrences_of_words.py
@@ -1,11 +1,23 @@
 """Program description"""
-words=[]
-word =""
-text = input("Enter text")
-for character in text:
-    if character != " ":
-        word += character
-    else:
-        words.append(word)
-        word = ""
-print(words)
+text = "this is a collection of words of nice words this is a fun thing it is"
+dict1 ={}
+words =  text.split()
+words_sorted = []
+
+for element in words:
+    dict1[element] = 0
+
+for element in words:
+    if element in dict1:
+        dict1[element] += 1
+# print (dict1)
+
+for key in dict1:
+    words_sorted.append(key)
+
+words_sorted.sort()
+
+print(words_sorted)
+
+for element in words_sorted:
+    print(element,":",dict1[element])
